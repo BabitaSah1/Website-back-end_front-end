3fd5155b5f174b0180322e0ecb2e8701d5466af2
Neetu-12 <neetus21@navgurukul.org>
2022-07-05 11:22:57 +0530

    Register,login with jwt and verify,validation,used next

---
+++ ROUTER_BY_ME/jwt.js
@@ -0,0 +1,29 @@
+const jwt=require("jsonwebtoken")
+
+var createToken=(data)=>{
+    // return jwt.sign(data,"secrete_Key")
+    token=jwt.sign(data,"secrete_Key")
+    console.log(token);//
+    return token
+}
+var verifyToken=(req,res,next)=>{
+    // console.log(req.headers)
+   var token=req.headers.cookie
+   if(token){
+        let tokens=token.split("=")[1]
+        console.log(tokens);
+       var verify=jwt.verify(tokens,"secrete_Key")
+       console.log(verify);//
+       if(!verify){
+        res.send("invalid token")
+       }
+       else{
+        req.body.meta=verify
+        next()
+       }
+   }
+   else{
+    res.send("login first")
+   }
+}
+module.exports={createToken,verifyToken}
